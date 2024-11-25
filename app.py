dc7bb282c37e4366405f9ef04ea27b4db16efc0f
Mega Nugraha <monmega110@gmail.com>
2024-11-25 15:54:30 +0700

    Change FastAPI server port from 5000 to 7976

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ async def predict(file: UploadFile = File(...)):
 # Start the FastAPI server
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv('PORT', '5000'))
+    port = int(os.getenv('PORT', '7976'))
     uvicorn.run(app, host='0.0.0.0', port=port)
