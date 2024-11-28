dab503387c936f0d39cd1b55470861b475a5c8b5
Mega Nugraha <monmega110@gmail.com>
2024-11-28 17:10:33 +0700

    add TF_ENABLE_ONEDNN_OPTS environment variable to config.py to disable said flag

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ load_dotenv()
 
 class Config:
     EXTRACTED_MODEL_DIR = os.path.join(os.getcwd(), 'model_dir')
+    TF_ENABLE_ONEDNN_OPTS = os.getenv('TF_ENABLE_ONEDNN_OPTS', '0')
+
+# Set the environment variable
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = Config.TF_ENABLE_ONEDNN_OPTS
