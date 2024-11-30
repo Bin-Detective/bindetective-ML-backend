d60e49ec968e21faab09870ed6a4701589eae1c1
Mega Nugraha <monmega110@gmail.com>
2024-12-01 02:43:48 +0700

    Update Dockerfile to use Python 3.9 and change FastAPI server port to 7976

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ async def predict(image_url: ImageURL):
 # Start the FastAPI server
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv('PORT', '8809'))
+    port = int(os.getenv('PORT', '7976'))
     uvicorn.run(app, host='0.0.0.0', port=port)
